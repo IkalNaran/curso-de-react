a12a2fb2b7d211fe802a3a3bbed190f7cf05a821
IkalNaran <ivanmauriciovt@gmail.com>
2025-11-24 17:37:51 -0600

    avance basico del curso 1 hora

---
+++ projects/01-holamundo/src/App.jsx
@@ -0,0 +1,10 @@
+import { Fragment } from 'react/jsx-runtime';
+import './App.css';
+import { TwiterFollowCard } from './TwiterFollowCard';
+export function App() {
+    return (
+        <>
+            <TwiterFollowCard />
+        </>
+    );
+}
---
+++ projects/01-holamundo/src/TwiterFollowCard.jsx
@@ -0,0 +1,15 @@
+export function TwiterFollowCard() {
+    <article className="md-tweetCard">
+            <header>
+                <img src="https://static0.srcdn.com/wordpress/wp-content/uploads/2025/05/one-piece-luffy-angry.jpg?q=70&fit=crop&w=1008&h=693&dpr=1" alt="" />
+                <div>
+                    <p>Ivan Mauricio Velazquez</p>  
+                    <span>@ikalnaran</span>
+                </div>
+            </header>
+
+            <aside>
+                <button>Seguir</button>
+            </aside>
+        </article>
+}
